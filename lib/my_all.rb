9ab951356723386b0ea14ff1cc5a5d91e1036c14
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-24 15:26:10 -0500

    commit tree fab5d192c5e7091f6b78d5f05e9c51ebe43752d1

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
-def my_all?(collection)
-
-end
+def my_all?(collection) # create method using that takes in arg (array)
+    i = 0 # begin counter since we are using a low level enum.
+    block_return_values = [] # want the elements called to be returned, need a new array
+    while i < collection.length # while, goes through each element in array
+      block_return_values << yield(collection[i]) # shovel new elements into new array
+      i = i + 1 # increment by 1 to go through all elements
+    end # close out while block
+  
+    if block_return_values.include?(false) # use include? to determine the return of my_all
+      false 
+    else # doesnt include false? then return true
+      true
+    end
+  end # close out entire method
